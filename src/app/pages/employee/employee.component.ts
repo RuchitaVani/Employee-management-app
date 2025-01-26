656394828b7a1b3b0ef85d586c6beb01f9523ea6
Ruchita Vani <56015636+RuchitaVani@users.noreply.github.com>
2025-01-26 00:12:18 -0700

    Dropdowns

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,25 +1,34 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MasterService } from '../../services/master.service';
-import { IApiResponse, IParentDept } from '../../model/Employee';
+import { IApiResponse, IChildDept, IParentDept } from '../../model/Employee';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './employee.component.html',
-  styleUrl: './employee.component.css'
+  styleUrl: './employee.component.css',
 })
 export class EmployeeComponent implements OnInit {
-parentDeptList : IParentDept[] = [];
+  parentDeptList: IParentDept[] = [];
+  childDeptList: IChildDept[] = [];
+  deptId: number = 0;
 
-masterService = inject(MasterService);
-ngOnInit(): void {
-  console.log(this.masterService);
-  this.getParentDeptList();
-
-}
-getParentDeptList(){
-  this.masterService.getParentDept().subscribe((res:IApiResponse)=>{
-this.parentDeptList =  res.data;
-  })
-}
+  masterService = inject(MasterService);
+  ngOnInit(): void {
+    console.log(this.masterService);
+    this.getParentDeptList();
+  }
+  getParentDeptList() {
+    this.masterService.getParentDept().subscribe((res: IApiResponse) => {
+      this.parentDeptList = res.data;
+    });
+  }
+  OnDeptChange() {
+    this.masterService
+      .getChildDeptByParentId(this.deptId)
+      .subscribe((res: IApiResponse) => {
+        this.childDeptList = res.data;
+      });
+  }
 }
